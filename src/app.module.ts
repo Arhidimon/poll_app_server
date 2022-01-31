c28dfc5952a0a3764a87be40fee431168dc60647
Dmitry <dbezruchenko@ex.ua>
2022-01-31 18:43:43 +0200

    Added reading MongoDB settings from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongodb/polls_app'),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
     AuthModule,
     UsersModule,
     PollsModule,
