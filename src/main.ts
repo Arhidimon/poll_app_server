1ff9dfa6d084c01bdc80c84eb697ba1d47c92df2
Dmitry <dbezruchenko@ex.ua>
2022-02-01 02:09:10 +0200

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
+const CORS_SETTINGS = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(CORS_SETTINGS);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
